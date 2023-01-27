faed6c553464c7d6650d0eddc1605c4296d695db
Suhail Kakar <suhail.zahed@gmail.com>
2023-01-27 17:06:20 +0530

    Update mint.ts

--- pages/api/mint.ts
+++ pages/api/mint.ts
@@ -1,21 +1,7 @@
-import { ThirdwebSDK } from "@thirdweb-dev/sdk/solana";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { address, metadata } = JSON.parse(req.body);
-
-  const sdk = ThirdwebSDK.fromPrivateKey(
-    "mainnet-beta",
-    process.env.PRIVATE_KEY as string
-  );
-
-  const collection = await sdk.getNFTCollection(
-    "GAjMpbjrAfm8uPNwQs5HfCWECCgi7ZGXnt4UhidVRkUm"
-  );
-
-  const nft = await collection.mintTo(address, metadata);
-
-  res.status(200).json({ nft });
+  res.send("gm!");
 };
 
 export default handler;
